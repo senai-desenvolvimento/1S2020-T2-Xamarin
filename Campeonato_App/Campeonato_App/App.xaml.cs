42721bd52fe635a1b82913363251e5882c4ada83
Erik Vitelli <erik.vitelli1@gmail.com>
2020-06-26 16:01:28 -0300

    up

---
+++ Campeonato_App/Campeonato_App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Campeonato_App.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Campeonato_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PrincipalView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
